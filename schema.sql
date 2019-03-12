11ada68dba11be38d214b8bbf8d157ba71eb0ca2
Angela Lee <rubii.kun@gmail.com>
2019-03-12 14:55:12 -0700

    Add schema table

--- schema.sql
+++ schema.sql
@@ -1,32 +1,18 @@
-DROP DATABASE IF EXISTS artist;
+DROP DATABASE IF EXISTS songs;
 
-CREATE DATABASE artist;
+CREATE DATABASE songs;
 
-USE artist;
-
-CREATE TABLE artist_info (
-  id INT(100) PRIMARY KEY AUTO_INCREMENT,
-  artist_name TEXT(150),
-  avatar_picture TEXT(200),
-  no_of_followers INT(20),
-  no_of_tracks INT(20),
-  pro_status INT(1), 
-  is_followed INT(1)
-);
+USE songs;
 
 CREATE TABLE song_description (
-  id INT(100) PRIMARY KEY AUTO_INCREMENT,
-  song_name TEXT(200),
-  artist_name TEXT(200), 
-  licence TEXT(200), 
-  description_text TEXT(4000), 
-  released_by TEXT(200), 
+  id SERIAL PRIMARY KEY,
+  unique_id INT,
+  song_name VARCHAR(200),
+  artist_name VARCHAR(200), 
+  licence VARCHAR(200), 
+  description_text VARCHAR(4000), 
+  released_by VARCHAR(200), 
   release_date DATE, 
-  p_line TEXT(200), 
-  tags TEXT(200) 
+  p_line VARCHAR(200), 
+  tags VARCHAR(200) 
 )
-
--- const sqlsong = 'INSERT INTO song_description(songname, artistname, licence, descriptiontext, releasedby, releasedate, pline, tags ) VALUES ?';
--- //to run, open mysql 5.7 
--- and use the following command from the root directory:
--- source ./schema.sql; 
